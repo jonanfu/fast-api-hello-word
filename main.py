28704f607bbaf4cd966aa3b211ffb76fde9a88c1
Jona <jonanfu@gmail.com>
2021-10-29 15:22:28 -0500

    validation_query

--- main.py
+++ main.py
@@ -1,9 +1,43 @@
+#Python
+from typing import Optional
+
+#Pydantic
+from pydantic import BaseModel
+
+#FastAPI
 from fastapi import FastAPI
+from fastapi import Body
+from fastapi import Query
 
 app = FastAPI()
+
+#Models
+
+class Person(BaseModel):
+    first_name: str
+    last_name: str
+    age: int
+    hair_color: Optional[str] = None
+    is_married: Optional[bool] = None
+
+
 
 @app.get("/")
 def home():
     return {"Hola": "mundo"}
 
 
+#Request and Response Body
+
+@app.post("/person/new")
+def create_person(person: Person = Body(...)):
+    return person
+
+#Validaciones: Query Parameters
+
+@app.get("/person/detail")
+def show_person(
+    name: Optional[str] = Query(None, min_length=1, max_length=50),
+    age: str = Query(...)
+):
+    return {name: age}
